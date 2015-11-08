31ca5c12ac384cc23008100afc3d3555e2deb7b1
Nick Mai <nick.mai@outlook.com>
2015-11-08 10:05:47 -0800

    Fixed relative paths

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var semver = require('semver')
 
 // Perhaps there is a better way to confirm ES6 support.
 if (process.version && semver.gt(process.version, '4.0.0')) {
-  module.exports = require('src/potato.js')
+  module.exports = require('./src/potato.js')
 } else {
-  module.exports = require('build/potato.js')
+  module.exports = require('./build/potato.js')
 }
